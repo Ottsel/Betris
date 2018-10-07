3c587643fe904f4b0c1381079ae9704aa90ff983
Ottsel <nickhunter808@gmail.com>
2018-10-06 18:49:06 -0700

    Initial upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public GameObject[] Pieces;
+	
+	private void Start () {
+	
+		Instantiate(Pieces[Random.Range(0,Pieces.Length)]);
+	}
+}
